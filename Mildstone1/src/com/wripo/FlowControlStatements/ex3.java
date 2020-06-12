e35096f392b5a2f0c3bb96041d3e7a03a3f0f6e1
HAI <HAI@DESKTOP-SQKM9M1>
2020-06-12 09:12:21 +0530

    exercise 2_3

---
+++ Mildstone1/src/com/wripo/FlowControlStatements/ex3.java
@@ -0,0 +1,16 @@
+package com.wripo.FlowControlStatements;
+
+public class ex3 {
+	 public static void main (String[] args)
+	    {
+	             if (args.length ==0)
+	            {
+	            System.out.println("No Values");
+	    }
+	        else{
+	            for(String i : args)  
+	                System.out.print(i+" , ");
+	       
+	        }
+	        }
+}
