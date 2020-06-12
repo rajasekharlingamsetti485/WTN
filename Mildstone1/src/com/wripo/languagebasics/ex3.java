f35dd01b62c237ed8fdd572a209fc4e7ac86ee55
HAI <HAI@DESKTOP-SQKM9M1>
2020-06-12 07:49:24 +0530

    ex1_3

---
+++ Mildstone1/src/com/wripo/languagebasics/ex3.java
@@ -0,0 +1,12 @@
+package com.wripo.languagebasics;
+
+public class ex3 {
+	public static void main(String[] args)
+	{
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		int c=a+b;
+		System.out.printf("The sum of %d and %d is %d", a, b, c);
+	}
+
+}
