3f11d8229d63610cc5d24c4027b2402d15f69e3d
HAI <HAI@DESKTOP-SQKM9M1>
2020-06-12 08:25:10 +0530

    ex1_0

--- Mildstone1/src/com/wripo/FlowControlStatements/ex1.java
+++ Mildstone1/src/com/wripo/FlowControlStatements/ex1.java
@@ -1,5 +1,24 @@
 package com.wripo.FlowControlStatements;
 
 public class ex1 {
+	 public static void main(String[] args) 
+	    {
+	        int number=Integer.parseInt(args[0]);;
+	     
+	        System.out.print("Enter the number you want to check:");
+	     
+	        if(number > 0)
+	        {
+	            System.out.println(number+" is positive number");
+	        }
+	        else if(number < 0)
+	        {
+	            System.out.println(number+" is negative number");
+	        }
+	        else
+	        {
+	            System.out.println(number+" is Zero");
+	        }
+	    }
 
 }
