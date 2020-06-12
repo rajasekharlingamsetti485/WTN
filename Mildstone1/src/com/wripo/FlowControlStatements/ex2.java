84cf5d81db74555c330aa96acc4b1dcfdf83cf84
HAI <HAI@DESKTOP-SQKM9M1>
2020-06-12 08:56:10 +0530

    Exercise 2_2

--- Mildstone1/src/com/wripo/FlowControlStatements/ex2.java
+++ Mildstone1/src/com/wripo/FlowControlStatements/ex2.java
@@ -1,5 +1,16 @@
 package com.wripo.FlowControlStatements;
 
 public class ex2 {
-
+	 public static void main(String[] args) 
+	    {
+	        int n=Integer.parseInt(args[0]);
+	        if(n % 2 == 0)
+	        {
+	            System.out.println(+n+" is Even ");
+	        }
+	        else
+	        {
+	            System.out.println(+n+" is Odd ");
+		}
+	    }
 }
