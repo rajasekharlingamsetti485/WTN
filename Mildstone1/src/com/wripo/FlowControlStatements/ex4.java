0264517858bbf847c6744df884474434956aa090
HAI <HAI@192.168.0.6>
2020-06-13 08:12:49 +0530

    exercise 2_4

--- Mildstone1/src/com/wripo/FlowControlStatements/ex4.java
+++ Mildstone1/src/com/wripo/FlowControlStatements/ex4.java
@@ -1,5 +1,16 @@
 package com.wripo.FlowControlStatements;
 
 public class ex4 {
+	    public static void main(String args[]){
+	        char a = args[0].charAt(0);
+	        char d = args[1].charAt(0);
+	        if(a<d){
+	            System.out.println(args[0]+" "+args[1]);
+	        }
+	        else{
+	            System.out.println(args[1]+" "+args[0]);
+	        }
+	    }
 
-}
+	}
+
