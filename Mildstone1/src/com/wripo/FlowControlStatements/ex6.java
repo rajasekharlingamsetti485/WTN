f1ec4fa4d71580eba017b077d0d2511cedb56d1b
HAI <HAI@192.168.0.6>
2020-06-13 08:17:27 +0530

    exercise 2_6

--- Mildstone1/src/com/wripo/FlowControlStatements/ex6.java
+++ Mildstone1/src/com/wripo/FlowControlStatements/ex6.java
@@ -1,5 +1,20 @@
 package com.wripo.FlowControlStatements;
 
 public class ex6 {
-
+	 public static void main(String args[]){
+	        String g=args[0];
+	        int age= Integer.parseInt(args[1]);
+	        if(g.equals("Female") && age>=1 && age<=58){
+	            System.out.println("percentage of interest is 8.2%");
+	        }
+	        else if(g.equals("Female") && age>=59 && age<=100){
+	            System.out.println("percentage of interest is 9.2%");
+	        }
+	        else if(g.equals("Male") && age>=1 && age<=58){
+	            System.out.println("percentage of interest is 8.4%");
+	        }
+	        else if(g.equals("Male") && age>=59 && age<=100){
+	            System.out.println("percentage of interest is 10.5%");
+	        }
+	    }
 }
