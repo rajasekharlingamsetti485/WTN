36aa8ea5cf32ffa58cdde0c8ec0dd4563836c774
HAI <HAI@192.168.0.6>
2020-06-13 08:13:57 +0530

    exercise 2_5

--- Mildstone1/src/com/wripo/FlowControlStatements/ex5.java
+++ Mildstone1/src/com/wripo/FlowControlStatements/ex5.java
@@ -1,5 +1,17 @@
 package com.wripo.FlowControlStatements;
 
 public class ex5 {
+	 public static void main(String args[]){
+	        char c='4';
 
+	        if((c>='a'&& c<='z')||(c>='A' &&c<='Z')){
+	            System.out.println("Alphabet");
+	        }
+	        else if((c>=32 && c<= 47)|| (c>=58 && c<= 64)||(c>=91 && c<= 96)||(c>=123 && c<= 126)){
+	            System.out.println("Special Character");
+	        }
+	        else{
+	            System.out.println("Digit");
+	        }
+	    }
 }
