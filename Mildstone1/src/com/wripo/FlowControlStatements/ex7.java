1e8c94d484d1e8276679ebb10e16bdfe48c21370
HAI <HAI@192.168.0.6>
2020-06-13 08:24:45 +0530

    jdbk

--- Mildstone1/src/com/wripo/FlowControlStatements/ex7.java
+++ Mildstone1/src/com/wripo/FlowControlStatements/ex7.java
@@ -1,5 +1,17 @@
 package com.wripo.FlowControlStatements;
 
 public class ex7 {
+	public static void main(String args[]){
+        char c= args[0].charAt(0);
+        int d;
+        if(c>='a'&&c<='z')
+            d= c-'a'+ 65;
+        else
+            d=c-'A'+97;
+        char a= (char)d;
+
+        System.out.println(a);
+    }
+
 
 }
