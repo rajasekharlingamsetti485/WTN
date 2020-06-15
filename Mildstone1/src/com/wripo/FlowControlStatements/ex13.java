521f4e7bf8f94c0fadc6de9d5cab095c214dbd12
HAI <HAI@192.168.0.6>
2020-06-15 07:57:05 +0530

    exercise 2_13

--- Mildstone1/src/com/wripo/FlowControlStatements/ex13.java
+++ Mildstone1/src/com/wripo/FlowControlStatements/ex13.java
@@ -3,13 +3,27 @@ package com.wripo.FlowControlStatements;
 public class ex13 {
 	public static void main(String args[])
 	{
-		for(int i=23;i<=57;i++)
-		{
-			if(i%2==0)
-			{
-				System.out.println(i);
-			}
-		}
+		 int i=0;
+	     int a=0;
+	     String  prime="";
+
+	       for (i=10;i<=99;i++)         
+	       { 		  	  
+	          int count=0; 	  
+	          for(a=i;a>=1;a--)
+		  {
+	             if(i%a==0)
+		     {
+	 		count=count+1;
+		     }
+		  }
+		  if(count==2)
+		  {
+		     prime=prime+ i + " ";
+		  }	
+	       }	
+	       System.out.println(prime);
 	}
+
 
 }
