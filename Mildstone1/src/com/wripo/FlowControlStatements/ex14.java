69710790269d681a8a77aa6d23da3db72f01b9e4
HAI <HAI@192.168.0.6>
2020-06-15 08:07:46 +0530

    exercise 2_14

--- Mildstone1/src/com/wripo/FlowControlStatements/ex14.java
+++ Mildstone1/src/com/wripo/FlowControlStatements/ex14.java
@@ -2,4 +2,15 @@ package com.wripo.FlowControlStatements;
 
 public class ex14 {
 
+	public static void main(String[] args) {
+		int i,sum=0;
+		int n=346;
+		for(i=0;n!=0;n=n/10)
+		{
+			sum=sum+(n%10);
+		}
+		System.out.println(sum);
+
+	}
+
 }
