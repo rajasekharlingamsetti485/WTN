752ed8aabebfc95f06001bbf0b2f21010dd77ace
HAI <HAI@192.168.0.6>
2020-06-17 07:42:13 +0530

    exercise 3_6

--- Mildstone1/src/com/wripo/Arrays/ex6.java
+++ Mildstone1/src/com/wripo/Arrays/ex6.java
@@ -1,5 +1,24 @@
 package com.wripo.Arrays;
 
 public class ex6 {
-
+	public static void main(String[] args) {
+		int i,t=0,j;
+		int[] a=new int[args.length];
+		for( i=0;i<args.length;i++)
+		   a[i]=Integer.parseInt(args[i]);
+		for(i=0;i<args.length;i++)
+		{
+		for(j=i+1;j<args.length;j++)
+		{
+		if(a[i]>a[j])
+		{
+		t=a[i];
+		a[i]=a[j];
+		a[j]=t;
+		}
+		}
+		}
+		for( i=0;i<args.length+1;i++)
+		   System.out.println(a[i]);
+	}
 }
