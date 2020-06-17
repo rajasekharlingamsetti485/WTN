ffeb5776d5d9e874753e125a2972d247a6ec9c9f
HAI <HAI@192.168.0.6>
2020-06-17 07:05:12 +0530

    exercise 2_16

--- Mildstone1/src/com/wripo/FlowControlStatements/ex16.java
+++ Mildstone1/src/com/wripo/FlowControlStatements/ex16.java
@@ -1,5 +1,16 @@
 package com.wripo.FlowControlStatements;
 
 public class ex16 {
+	public static void main(String[] args) {
+		int num=1234, rev = 0;
+        while(num != 0) {
+            int digit = num % 10;
+            rev = rev * 10 + digit;
+            num /= 10;
+        }
+
+        System.out.println("" + rev);
+
+	}
 
 }
