71b3456caa49ef2ff2ad8245c7285922b6424da0
HAI <HAI@192.168.0.6>
2020-06-17 07:54:55 +0530

    exercise 3_11

--- Mildstone1/src/com/wripo/Arrays/ex11.java
+++ Mildstone1/src/com/wripo/Arrays/ex11.java
@@ -1,5 +1,17 @@
 package com.wripo.Arrays;
 
 public class ex11 {
-
+	public static void main(String[] args) {
+		int i,c=0;
+		for(i=0;i<args.length;i++)
+		{
+			int x=Integer.parseInt(args[i]);
+			if(x!=1&&x!=4)
+				c++;
+		}
+		if(c!=0)
+			System.out.println("False");
+		else
+			System.out.println("True");
+	}
 }
