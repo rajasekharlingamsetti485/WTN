3e29192d02fe0bced453e23b31d8adaa560daeee
HAI <HAI@192.168.0.6>
2020-06-17 07:20:29 +0530

    exercise 2_17

---
+++ Mildstone1/src/com/wripo/FlowControlStatements/ex17.java
@@ -0,0 +1,20 @@
+package com.wripo.FlowControlStatements;
+
+public class ex17 {
+	public static void main(String[] args) {
+		int n=Integer.parseInt(args[0]);
+		int r,q=0;
+		int x=n;
+		while(n>0)
+		{
+			r=n%10;
+			q=q*10+r;
+			n/=10;
+		}
+		if(x==q)
+		{
+			
+		}
+		
+}
+}
