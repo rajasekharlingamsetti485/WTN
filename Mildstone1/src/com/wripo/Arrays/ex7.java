fd91d2393fe7d2166f14d134d1a1adac65174533
HAI <HAI@192.168.0.6>
2020-06-17 07:44:28 +0530

    exercise 3_7

--- Mildstone1/src/com/wripo/Arrays/ex7.java
+++ Mildstone1/src/com/wripo/Arrays/ex7.java
@@ -1,5 +1,22 @@
 package com.wripo.Arrays;
 
 public class ex7 {
-
+	public static void main(String[] args) {
+		int i;
+		int a[]= new int[args.length];
+		for(i=0;i<args.length;i++)
+		{
+			a[i]=0;
+		}
+		for(i=0;i<args.length;i++)
+		{
+			int x=Integer.parseInt(args[i]);
+			a[x]++;
+		}
+		for(i=0;i<args.length;i++)
+		{
+			if(a[i]!=0)
+				System.out.println(i);
+		}
+	}
 }
