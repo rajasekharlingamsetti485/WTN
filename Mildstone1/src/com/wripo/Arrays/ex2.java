a25220ef1c2fd10174d2eb23058cafeab67dd266
HAI <HAI@192.168.0.6>
2020-06-17 07:33:06 +0530

    exercise 3_2

--- Mildstone1/src/com/wripo/Arrays/ex2.java
+++ Mildstone1/src/com/wripo/Arrays/ex2.java
@@ -1,5 +1,17 @@
 package com.wripo.Arrays;
 
 public class ex2 {
-
+	public static void main(String[] args) {
+		int max=Integer.parseInt(args[0]);
+		int min=Integer.parseInt(args[0]);
+		for(int i=0;i<args.length;i++)
+		{
+			int v=Integer.parseInt(args[i]);
+			if(max<v)
+				max=v;
+			if(min>v)
+				min=v;
+		}
+		System.out.println(min+","+max);
+	}
 }
