cd5fb6357ca134a20f908582e1568beec22951b7
HAI <HAI@192.168.0.6>
2020-06-17 07:56:55 +0530

    exercise 3_12

--- Mildstone1/src/com/wripo/Arrays/ex12.java
+++ Mildstone1/src/com/wripo/Arrays/ex12.java
@@ -1,5 +1,19 @@
 package com.wripo.Arrays;
 
 public class ex12 {
-
+	public static void main(String[] args) {
+		int k=args.length;
+		int x=0,y=0,c=k/2;
+		if(c%2==0)
+			{
+				x=Integer.parseInt(args[c/2]);
+				y=Integer.parseInt(args[c+(c/2)]);
+			}
+		else if((k/2)%2!=0)
+			{
+				x=Integer.parseInt(args[c/2]);
+				y=Integer.parseInt(args[c+c/2]);
+			}
+		System.out.println(x+","+y);
+	}
 }
