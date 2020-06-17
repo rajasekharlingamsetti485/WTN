4c60a6589cf910efc8f08cb2466de2486eeb0686
HAI <HAI@192.168.0.6>
2020-06-17 07:49:20 +0530

    exercise 3_9

--- Mildstone1/src/com/wripo/Arrays/ex9.java
+++ Mildstone1/src/com/wripo/Arrays/ex9.java
@@ -1,5 +1,19 @@
 package com.wripo.Arrays;
 
 public class ex9 {
-
+	public static void main(String[] args) {
+		int i,c=0;
+		for(i=0;i<args.length;i++)
+		{
+			int x=Integer.parseInt(args[i]);
+			if(x==10)
+				c++;
+			else
+				System.out.print(x+" ");
+		}
+		for(i=0;i<c;i++)
+		{
+			System.out.print("0 ");
+		}
+	}
 }
