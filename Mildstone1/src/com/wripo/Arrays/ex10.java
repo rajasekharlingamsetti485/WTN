0786546b2511e0b6e420fe458bd6903264444da9
HAI <HAI@192.168.0.6>
2020-06-17 07:52:44 +0530

    exercise 3_10

--- Mildstone1/src/com/wripo/Arrays/ex10.java
+++ Mildstone1/src/com/wripo/Arrays/ex10.java
@@ -1,5 +1,29 @@
 package com.wripo.Arrays;
 
 public class ex10 {
-
+	public static void main(String[] args) {
+		int i;
+		int a[]=new int [args.length];
+		for(i=0;i<args.length;i++)
+		{
+			int x= Integer.parseInt(args[i]);
+			a[i]=x;
+		}
+		for(i=0;i<args.length;i++)
+		{
+			if(a[i]%2==0)
+			{
+				System.out.print(a[i]+" ");
+				a[i]=0;
+			}
+		}
+		for(i=0;i<args.length;i++)
+		{
+			if(a[i]!=0)
+			{
+				System.out.print(a[i]+" ");
+			}
+		}
+	}
 }
+
