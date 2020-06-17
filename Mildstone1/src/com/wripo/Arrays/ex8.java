a63c7e6460072457199f91438323df23290c0a7d
HAI <HAI@192.168.0.6>
2020-06-17 07:46:32 +0530

    exercise 3_8

--- Mildstone1/src/com/wripo/Arrays/ex8.java
+++ Mildstone1/src/com/wripo/Arrays/ex8.java
@@ -1,5 +1,26 @@
 package com.wripo.Arrays;
 
 public class ex8 {
-
+	public static void main(String[] args) {
+		int i,c=0,d=0,s=0,v=0;
+		for(i=0;i<args.length;i++)
+		{
+			int x=Integer.parseInt(args[i]);
+			if(x==6)
+				c=i;
+			if(x==7)
+				d=i;
+			s+=x;
+		}
+		if(c<d)
+		{
+			for(i=c;i<=d;i++)
+			{
+				int x=Integer.parseInt(args[i]);
+				v+=x;
+			}
+			s-=v;
+		}
+		System.out.println(s);
+	}
 }
