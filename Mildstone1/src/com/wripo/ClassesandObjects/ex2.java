d1cc9b36cd497fde4314a6ade222fbbe6a8594c5
HAI <HAI@192.168.0.3>
2020-06-24 08:44:01 +0530

    exercise 3_2

--- Mildstone1/src/com/wripo/ClassesandObjects/ex2.java
+++ Mildstone1/src/com/wripo/ClassesandObjects/ex2.java
@@ -1,5 +1,21 @@
 package com.wripo.ClassesandObjects;
 
 public class ex2 {
+	public class calci{
+		public static void main(String[] args) {
+			System.out.println(calculator.powerInt(10,2));
+			System.err.println(calculator.powerDouble(10.0,2));
+		}
+	}
 
+	class calculator{
+		static double powerDouble(double num1,double num2)
+		{
+		  return Math.pow(num1, num2);	
+		}
+		static int powerInt(int num1,int num2)
+		{
+		  return (int)Math.pow(num1, num2);	
+		}
+	}
 }
