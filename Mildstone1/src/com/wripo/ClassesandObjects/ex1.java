1f421a48aaff188ecd64a5987b3681444086042b
HAI <HAI@192.168.0.3>
2020-06-24 08:39:42 +0530

    exercise 3_1

--- Mildstone1/src/com/wripo/ClassesandObjects/ex1.java
+++ Mildstone1/src/com/wripo/ClassesandObjects/ex1.java
@@ -1,5 +1,26 @@
 package com.wripo.ClassesandObjects;
 
 public class ex1 {
-
+	class Cuboid {
+		private int width;
+		private int depth;
+		private int hieght;
+		Cuboid(int width,int hieght, int depth)
+		{
+			this.width=width;
+			this.hieght=hieght;
+			this.depth=depth;
+		}
+		public double Volume ()
+		{
+		 return width*hieght*depth;
+		}
+		}
+		public class Box {
+			public static void main(String[] args) {
+				Cuboid sc=new Cuboid(10,20,30);
+				double volume= sc.Volume();
+				System.out.println(volume);
+			}
+		}
 }
