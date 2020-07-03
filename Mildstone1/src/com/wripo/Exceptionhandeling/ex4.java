0b8bc8bea4d4676150c0552f7ef758258ba685c4
HAI <HAI@192.168.0.4>
2020-07-03 23:30:43 +0530

    exercise 9_4

--- Mildstone1/src/com/wripo/Exceptionhandeling/ex4.java
+++ Mildstone1/src/com/wripo/Exceptionhandeling/ex4.java
@@ -1,5 +1,30 @@
 package com.wripo.Exceptionhandeling;
+class ex4 {
+	
+	public void registerUser(String username, String userCountry) throws InvalidCountryException {
+		if (!userCountry.equals("India"))
+			throw new InvalidCountryException();
+		else
+			System.out.println("User registration done successfully");
+		
+		
+	}
 
-public class ex4 {
+	public static void main(String[] args) {
+		ex4 registration = new ex4 ();
+		
+		try {
+			registration.registerUser("Rohith", "India");
+		} catch (InvalidCountryException e) {
+		}
+	}
 
 }
+class InvalidCountryException extends Exception {
+	public InvalidCountryException() {
+		super();
+		System.out.println("InvalidCountryException occured");
+		System.out.println("User Outside India  cannot be registered");
+	}
+}
+
