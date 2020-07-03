e19eedcfd83f7e61959104e6d39d7fef260d2087
HAI <HAI@192.168.0.4>
2020-07-03 23:17:05 +0530

    exercise 8_1

--- Mildstone1/src/com/wripo/interfaces/ex1.java
+++ Mildstone1/src/com/wripo/interfaces/ex1.java
@@ -1,5 +1,30 @@
 package com.wripo.interfaces;
-
-public class ex1 {
-
+class ex1 {
+    public static void main(String args[]) {
+        Veena v = new Veena();
+        Saxophone s = new Saxophone();
+        Playable pv, ps;
+        pv = new Veena();
+        ps = new Saxophone();
+        
+        v.play(); 
+        s.play(); 
+        pv.play();
+        ps.play();
+        
+    }
 }
+class Veena implements Playable {
+	public void play() {
+        System.out.println("Violin is being played");
+    }
+}
+class Saxophone implements Playable {
+	public void play() {
+        System.out.println("Saxophone is being played");
+    }
+}
+interface Playable {
+	public void play();
+}
+
