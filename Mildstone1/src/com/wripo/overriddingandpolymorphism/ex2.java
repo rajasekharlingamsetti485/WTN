61d78775ee47e37f232ab0ea8db834a6b82f3d9e
HAI <HAI@192.168.0.4>
2020-07-03 23:08:56 +0530

    exercise 6_2

--- Mildstone1/src/com/wripo/overriddingandpolymorphism/ex2.java
+++ Mildstone1/src/com/wripo/overriddingandpolymorphism/ex2.java
@@ -1,5 +1,57 @@
 package com.wripo.overriddingandpolymorphism;
-
 public class ex2 {
+	  public static void main(String[] args) {
+		  ex2 c=new Circle();
+		  ex2  t=new Triangle();
+		  ex2  s=new Square();
+	    c.draw();
+	    c.erase();
+	    t.draw();
+	    t.erase();
+	    s.draw();
+	    s.erase();
+	    
+	  }
+	void draw()
+	{
+	  System.out.println("Drawing Shape");
+	}
+	void erase()
+	{
+	  System.out.println("Erasing Shape");
+	}
 
-}
+	}
+
+	class Circle extends ex2 {
+	  void draw()
+	  {
+	    System.out.println("Drawing circle");
+	  }
+	  void erase()
+	  {
+	    System.out.println("Erasing Cirle");
+	  }
+	}
+
+	class Triangle extends ex2 {
+	  void draw()
+	  {
+	    System.out.println("Drawing Triangle");
+	  }
+	  void erase()
+	  {
+	    System.out.println("Erasing Triangle");
+	  }
+	}
+
+	class Square extends ex2 {
+	  void draw()
+	  {
+	    System.out.println("Drawing Square");
+	  }
+	  void erase()
+	  {
+	    System.out.println("Erasing Square");
+	  }
+	}
