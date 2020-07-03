59ffb7f90ce1be3b8d13599c13141a423f168d26
HAI <HAI@192.168.0.4>
2020-07-03 23:04:32 +0530

    exercise 6_1

--- Mildstone1/src/com/wripo/overriddingandpolymorphism/ex1.java
+++ Mildstone1/src/com/wripo/overriddingandpolymorphism/ex1.java
@@ -1,5 +1,38 @@
 package com.wripo.overriddingandpolymorphism;
 
+class Apple extends Fruit {
+	public void eat() {
+		System.out.println("It tastes like apple");
+	}
+}
+class Fruit {
+	protected String name;
+	protected String taste;
+	protected int size;
+	
+	public Fruit() {
+		name = "Fruit";
+		taste = "Taste of the fruit";
+		size = 0;
+	}
+	
+	public void eat () {
+		System.out.println(name + " tastes like " + taste);
+	}
+}
+
+class Orange extends Fruit {
+	public void eat() {
+		System.out.println("It tastes like Orange");
+	}
+}
 public class ex1 {
+
+	public static void main(String[] args) {
+		new Fruit().eat();
+		new Apple().eat();
+		new Orange().eat();
+
+	}
 
 }
