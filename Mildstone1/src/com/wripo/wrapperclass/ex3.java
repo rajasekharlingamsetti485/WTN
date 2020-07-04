c6543250b70ad78e0cf219c120dc9632af9894ae
HAI <HAI@192.168.0.4>
2020-07-04 08:46:29 +0530

    exercise 10_1 exercise 10_2 exercise 10_3

--- Mildstone1/src/com/wripo/wrapperclass/ex3.java
+++ Mildstone1/src/com/wripo/wrapperclass/ex3.java
@@ -1,5 +1,12 @@
 package com.wripo.wrapperclass;
 
+import java.util.Scanner;
 public class ex3 {
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);		
+		int input = sc.nextInt();		
+		String output = String.format("%8s", Integer.toBinaryString(input)).replace(' ', '0');
+		System.out.println(output);		
+		sc.close();
+	}
 }
