97a8b0fcee41c2a328fcfc446a4c1fd46b060576
HAI <HAI@192.168.0.4>
2020-07-04 07:47:32 +0530

    ex3

--- Mildstone1/src/com/wripo/day7/ex3.java
+++ Mildstone1/src/com/wripo/day7/ex3.java
@@ -1,5 +1,20 @@
 package com.wripo.day7;
+class ex3
+{
+public int uniqueDigitsCount(int input1){
 
-public class ex3 {
-
+int c=0,r,i;
+int h[]=new int[10];
+while(input1>0){
+r=input1%10;
+h[r]++;
+input1=input1/10;
+}
+for(i=0;i<10;i++){
+if(h[i]>0){
+c++;
+}
+}
+return c;
+}
 }
