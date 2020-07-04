4a06115516fb40b333f02547907c01b4e1622ccd
HAI <HAI@192.168.0.4>
2020-07-04 07:33:46 +0530

    ex1

--- Mildstone1/src/com/wripo/day10/ex1.java
+++ Mildstone1/src/com/wripo/day10/ex1.java
@@ -1,5 +1,14 @@
 package com.wripo.day10;
-
-public class ex1 {
-
+public class ex1
+{
+public String secondWordUpperCase(String input1)
+{
+String s[]=input1.split(" ");
+if(s.length==1)
+return "LESS";
+String s1=s[1];
+s1=s1.toUpperCase();
+return s1;
 }
+}
+
