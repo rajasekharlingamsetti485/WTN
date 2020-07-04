2dff44f39c471e7c7c173e312fab079e5bcfbd8f
HAI <HAI@192.168.0.4>
2020-07-04 06:54:41 +0530

    ex1

--- Mildstone1/src/com/wripo/day6/ex1.java
+++ Mildstone1/src/com/wripo/day6/ex1.java
@@ -1,5 +1,17 @@
 package com.wripo.day6;
-
-public class ex1 {
-
+import java.io.*;
+import java.util.*;
+class ex1
+{
+public int isPrime(int input1){
+int cnt=0;
+for(int i=1;i<=input1;i++){
+if(input1%i==0) cnt++;
 }
+if(cnt==2) return 2;
+else return 1;
+}
+}
+
+
+
