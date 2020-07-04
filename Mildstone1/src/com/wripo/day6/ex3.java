e97fd41543591bfe8a7af59d66b8e43d5f1972e8
HAI <HAI@192.168.0.4>
2020-07-04 06:59:54 +0530

    ex3

--- Mildstone1/src/com/wripo/day6/ex3.java
+++ Mildstone1/src/com/wripo/day6/ex3.java
@@ -1,5 +1,19 @@
 package com.wripo.day6;
-
-public class ex3 {
-
+import java.io.*;
+import java.util.*;
+class ex3
+{
+public long nthFibonacci(int input1){
+int a=0;
+int b=1;
+int c=0;
+int d=3;
+while(d<=input1){
+ c=a+b;
+a=b;
+b=c;
+d++;
+}
+return c;
+}
 }
