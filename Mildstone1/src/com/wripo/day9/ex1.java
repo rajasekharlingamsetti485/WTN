128bed86f90b19245049c374713654117e2e8bf5
HAI <HAI@192.168.0.4>
2020-07-04 07:10:55 +0530

    ex1

--- Mildstone1/src/com/wripo/day9/ex1.java
+++ Mildstone1/src/com/wripo/day9/ex1.java
@@ -1,5 +1,18 @@
 package com.wripo.day9;
-
-public class ex1 {
-
-}
+class ex1{  
+	 public static void main(String args[]){  
+	  int r,sum=0,temp;    
+	  int n=454;
+	  
+	  temp=n;    
+	  while(n>0){    
+	   r=n%10;  
+	   sum=(sum*10)+r;    
+	   n=n/10;    
+	  }    
+	  if(temp==sum)    
+	   System.out.println("palindrome number ");    
+	  else    
+	   System.out.println("not palindrome");    
+	}  
+	}  
